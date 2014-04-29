98ae69d052314e2fc3efbe25588a08dc469d0f4a
azulena218 <azulena.royer@uvm.edu>
2014-04-29 17:07:14 -0400

    Game class

---
+++ Game.java
@@ -0,0 +1,20 @@
+/**
+   Azulena Royer
+   CS 110
+   Assignment 10: Game Class
+   The Game class simulates a game of War 
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;  
+
+public class Game
+{
+	public static void main(String[] args)
+	{
+		JFrame f = new WarGUI();
+		f.setSize(1200, 900);
+      f.setVisible(true);
+	}
+}
